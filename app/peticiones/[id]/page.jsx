16a9e4d471ff60d72cbdee7e5cccfb155d004b37
AxelSparta <axelnicolassparta@gmail.com>
2025-02-12 14:20:27 -0300

    dinamic pages

---
+++ app/peticiones/[id]/page.jsx
@@ -0,0 +1,4 @@
+export default function PeticionesId({params}) {
+	console.log(params.id)
+	return <h1>PeticionesId {params.id}</h1>
+}
--- app/peticiones/page.jsx
+++ app/peticiones/page.jsx
@@ -1,19 +1,20 @@
+import PostCard from "@/components/PostCard"
+
 const getPosts = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts')
   const data = await response.json()
   return data
 }
-export default async function Peticiones () {
+export default async function PeticionesPage () {
   const posts = await getPosts()
   return (
     <div>
       <h1>Peticiones</h1>
-      {posts.map(post => (
-        <div className="grid grid-cols-3 gap-4 " key={post.id}>
-          <h2 className="text-xl">{post.title}</h2>
-          <p className="text-sm">{post.body}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-5 mx-auto">
+        {posts.map(post => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   )
 }
