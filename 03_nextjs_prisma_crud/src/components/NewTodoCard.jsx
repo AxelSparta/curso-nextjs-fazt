88dd26e4329e2686fa31093970c0b84a55d41b17
AxelSparta <axelnicolassparta@gmail.com>
2025-02-20 21:00:19 -0300

    03 adding task form

--- 03_nextjs_prisma_crud/src/components/NewTodoCard.jsx
+++ 03_nextjs_prisma_crud/src/components/NewTodoCard.jsx
@@ -1,14 +1,39 @@
 'use client'
 
-export default function NewTodoCard () {
+export default function NewTodoCard ({ updateTasks }) {
+  const handleSubmit = async e => {
+    try {
+      e.preventDefault()
+      // como es código cliente, no hace falta colocar toda la url, solo la ruta, si estuviésemos del lado server, habría que poner la url completa
+      const res = await fetch('/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title: e.target.title.value,
+          description: e.target.description.value
+        })
+      })
+      const data = await res.json()
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className='border rounded-md p-4 max-w-96 flex flex-col'>
+    <form
+      className='border rounded-md p-4 max-w-96 flex flex-col'
+      onSubmit={handleSubmit}
+    >
       <legend className='text-xl font-bold text-center mb-4'>
         Añadir tarea
       </legend>
       <fieldset className='mb-4'>
         <label htmlFor='title'>Título</label>
         <input
+          required
           type='text'
           id='title'
           name='title'
@@ -28,10 +53,9 @@ export default function NewTodoCard () {
       <button
         className='border rounded-md mx-auto p-2 bg-slate-100 text-slate-950 border-slate-100 font-semibold hover:text-slate-100 hover:bg-slate-950 transition-all'
         type='submit'
-        onSubmit={() => {}}
       >
         Añadir tarea
       </button>
-    </div>
+    </form>
   )
 }
